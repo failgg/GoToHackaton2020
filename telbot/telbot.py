8e8959b579b9ec323e0248a180c4ae3e4dcd5c35
DenisIndenbom <59681620+DenisIndenbom@users.noreply.github.com>
2020-01-11 19:32:21 +0300

    Update telbot.py

--- telbot/telbot.py
+++ telbot/telbot.py
@@ -7,7 +7,7 @@ import Smartload
 from telebot import types
 from telebot import apihelper
 
-token = ''
+token = '970309668:AAHZX5VTRWBDhZ6XUyNKIJP9W28dVOqGtjA'
 apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 bot = telebot.TeleBot(token=token, threaded=False)
 
@@ -19,10 +19,10 @@ def handle_text(message):
 @bot.message_handler(commands=["start"])
 def repeat_all_messages(message):
     keyboard = types.InlineKeyboardMarkup()
-    button1 = types.InlineKeyboardButton(text="FAQ", callback_data="help")
+    #button1 = types.InlineKeyboardButton(text="FAQ", callback_data="help")
     button2 = types.InlineKeyboardButton(text="Download photo", callback_data="send_image")
-    button3 = types.InlineKeyboardButton(text="Recognize", callback_data="recognize_image")
-    keyboard.add(button1)
+    button3 = types.InlineKeyboardButton(text="help", callback_data="help")
+    #keyboard.add(button1)
     keyboard.add(button2)
     keyboard.add(button3)
     bot.send_message(message.chat.id, "Use commands", reply_markup=keyboard)
@@ -30,16 +30,17 @@ def repeat_all_messages(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    if call.message:
-        if call.data == "help":
-            bot.send_message(call.message.chat.id, "Recognize Bakugans")
-            file_info = bot.get_file(handle_add_photo.file_id)
-            downloaded_file = bot.download_file(file_info.file_path)
-            bot.send_message(call.message.chat.id, "got")
-        elif call.data == "send_image":
+    #if call.message:
+        #if call.data == "help":
+         #   bot.send_message(call.message.chat.id, "Recognize Bakugans")
+          #  file_info = bot.get_file(handle_add_photo.file_id)
+           # downloaded_file = bot.download_file(file_info.file_path)
+            #bot.send_message(call.message.chat.id, "got")
+
+    if call.data == "send_image":
             msg = bot.send_message(call.message.chat.id, "Send me a photo")
             bot.register_next_step_handler(callback=handle_add_photo, message=msg)
-        elif call.data == "recognize_image":
+    elif call.data == "recognize_image":
             bot.send_message(call.message.chat.id, "already")
 
 
@@ -60,8 +61,10 @@ def handle_add_photo(message):
     # bot.send_message(message.chat.id, text)
     if ind == 0:
         bot.send_message(message.chat.id, "Centipoid")
+        print('Centipoid')
     elif ind == 1:
         bot.send_message(message.chat.id, "Blade Tigrerra")
+        print('Blade Tigrerra')
 
     # bot.send_photo(message.chat.id,downloaded_file)
 
